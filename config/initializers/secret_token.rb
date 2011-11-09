76a11c14d83368f9c96333540712bea52472454d
Fabio Luiz Nery de Miranda <fabio@miranti.net.br>
2011-11-09 00:18:18 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppContatosRails::Application.config.secret_token = '5371ce0d883615c5d1bea4fc945d820a3dc60bc09d0c8899f2b06c14a31405cb31041c7d673b802fcec5c8ae918245c419b731ce894f6a5c299c1fd9676482cc'
